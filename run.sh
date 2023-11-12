da18d97341f9bd2ebdf1d60366a8658be7cc4cea
petro-ew <v.petroew@gmail.com>
2023-11-12 22:57:01 +0300

    change run.sh add color lib for bash

--- run.sh
+++ run.sh
@@ -1,7 +1,29 @@
-#!/bin/sh
-cd ~/source/applegame/bin/
-rm applegame
-cd ~/source/applegame/build/
-cmake --build ..
-cd ~/source/applegame/bin/
-./applegame
+#!/bin/bash
+
+#include colors lib
+source $HOME/source/applegame/lib/colors.sh
+
+bin_name=applegame
+FILE=$HOME/source/applegame/build/bin/$bin_name
+#remove old bin file
+clear
+echo -e "${LYELLOW} remove bin files ${NORMAL}"
+rm $HOME/source/applegame/build/bin/$bin_name
+if test -f $HOME/source/applegame/build/bin/applegame
+then
+echo -e "${GREEN} $bin_name deleted. ${NORMAL}"
+else
+echo -e "${BGRED}${BLACK} $bin_name don't exists!${NORMAL}"
+fi 
+#build new version
+cd ~/source/applegame/build/ &&  cmake --build .
+if test -f $HOME/source/applegame/build/bin/applegame
+then
+echo -e "${GREEN} $FILE exists.${NORMAL}"
+cp -f $HOME/source/applegame/build/bin/applegame $HOME/source/applegame/bin/latest/
+# execute build version
+$HOME/source/applegame/build/bin/applegame
+else
+echo -e "${BGRED}${BLACK}Programs not compiling!!! $FILE NOT exists!!!${NORMAL}"
+fi
+
