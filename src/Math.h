9f4ab6ed613c9dbbe6d4aa10a5c8359d7e97607f
petro-ew <v.petroew@gmail.com>
2023-11-15 00:10:35 +0300

    add .clang-format file

--- src/Math.h
+++ src/Math.h
@@ -1,21 +1,20 @@
 #pragma once
 
-namespace ApplesGame
-{
+namespace ApplesGame {
 
-	struct Vector2D
-	{
-		float x = 0;
-		float y = 0;
-	};
+struct Vector2D {
+  float x = 0;
+  float y = 0;
+};
 
-	typedef Vector2D Position2D;
+typedef Vector2D Position2D;
 
-	Position2D GetRandomPositionInScreen(float screenWidth, float screenHeight);
+Position2D GetRandomPositionInScreen(float screenWidth, float screenHeight);
 
-	bool IsRectanglesCollide(Position2D rect1Position, Vector2D rect1Size,
-		Position2D rect2Position, Vector2D rect2Size);
+bool IsRectanglesCollide(Position2D rect1Position, Vector2D rect1Size,
+                         Position2D rect2Position, Vector2D rect2Size);
 
-	bool IsCirclesCollide(Position2D circle1Position, float circle1Radius,
-		Position2D circle2Position, float circle2Radius);
-}
+bool IsCirclesCollide(Position2D circle1Position, float circle1Radius,
+                      Position2D circle2Position, float circle2Radius);
+
+}        
