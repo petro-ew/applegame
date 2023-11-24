afab95041d1f4a973fbf1c8eb455372c199285da
petro-ew <v.petroew@gmail.com>
2023-11-25 00:52:19 +0300

    недолелка

--- src/State_Paused.h
+++ src/State_Paused.h
@@ -4,9 +4,32 @@
 
 #ifndef APPLEGAME_STATE_PAUSED_H
 #define APPLEGAME_STATE_PAUSED_H
+#pragma once
+#include "BaseState.h"
+#include "EventManager.h"
 
-class State_Paused
+class State_Paused : public BaseState
 {
+public:
+    State_Paused(StateManager* l_stateManager);
+    ~State_Paused();
+
+    void OnCreate();
+    void OnDestroy();
+
+    void Activate();
+    void Deactivate();
+
+    void Update(const sf::Time& l_time);
+    void Draw();
+
+    void Unpause(EventDetails* l_details);
+
+private:
+    sf::Font m_font;
+    sf::Text m_text;
+    sf::RectangleShape m_rect;
 };
+~
 
 #endif // APPLEGAME_STATE_PAUSED_H
